b13ef6cb82e797c198a50eb8a1ecf9d55e976557
nico <bobelet.nicolas@gmail.com>
2018-02-05 15:45:05 +0100

    Partie Controller

--- src/Controllers/PartieController.php
+++ src/Controllers/PartieController.php
@@ -14,43 +14,46 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 class PartieController extends BaseController
 {
 	public function getParties($request, $response, $args){
-
-		return $response->withJson(array('status' => '200 OK'));
+		$parties = Partie::select()->get();
+		$response = $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+		return $response->getBody()->write(json_encode($parties->toArray()));
 	}
 
 	public function getPartie($request,$response,$args) {
 		try {
 			$partie = Partie::where("id","=",$args['id'])->firstOrFail();
-		$response = $response->withHeader('Content-Type', 'application/json')->withStatus(200);
-        $response->getBody()->write(json_encode($partie->toArray()));
+			$response = $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+			return $response->getBody()->write(json_encode($partie->toArray()));
 			
 		} catch (ModelNotFoundException $exception){
 
-        $notFoundHandler = $this->container->get('notFoundHandler');
-        return $notFoundHandler($req,$resp);
-      }
+			$notFoundHandler = $this->container->get('notFoundHandler');
+			return $notFoundHandler($req,$resp);
+		}
 
 	}
 
 	public function createPartie($request,$response) {
-		 $tab = $req->getParsedBody();
-      $partie = new Partie();
-      $partie->joueur = filter_var($tab["joueur"],FILTER_SANITIZE_STRING);
-      $partie->score = 0;
-      $partie->serie_id = filter_var($tab["serie_id"],FILTER_SANITIZE_NUMBER_FLOAT);
-      $partie->state =0;   
+		$tab = $request->getParsedBody();
+		$partie = new Partie();
+		$partie->player_username = filter_var($tab["player_username"],FILTER_SANITIZE_STRING);
+		$partie->score = 0;
+		$partie->nb_photos=filter_var($tab["nb_photos"],FILTER_SANITIZE_NUMBER_FLOAT);
+		$partie->serie_id = filter_var($tab["serie_id"],FILTER_SANITIZE_NUMBER_FLOAT);
+		      $partie->token = bin2hex(openssl_random_pseudo_bytes(32));
+		$partie->state =0;   
 
-      try{
-      $partie->save();
-        $response = $response->withHeader('Content-Type', 'application/json')->withStatus(201);
-        $response->getBody()->write(json_encode($partie->toArray()));
-        return $response;
+		try{
+			$partie->save();
+			$response = $response->withHeader('Content-Type', 'application/json')->withStatus(201);
+			$response->getBody()->write(json_encode($partie->toArray()));
+			return $response;
 
-      } catch (\Exception $e){
-        $response = $response->withHeader('Content-Type','application/json')->withStatus(500);
-        $response->getBody()->write(json_encode(['type' => 'error', 'error' => 500, 'message' => $e->getMessage()]));
+		} catch (\Exception $e){
+			$response = $response->withHeader('Content-Type','application/json')->withStatus(500);
+			$response->getBody()->write(json_encode(['type' => 'error', 'error' => 500, 'message' => $e->getMessage()]));
 
-      }
-    }
+		}
+	}
 	
 }
--- src/includes/api/routes.php
+++ src/includes/api/routes.php
@@ -1,3 +1,5 @@
 <?php
-
+// Routes Parties
 	$app->get('/parties[/]', 'PartieController:getParties')->setName('get_parties');
+	$app->get('/parties/{id}[/]', 'PartieController:getPartie')->setName('get_partie');
+	$app->post('/parties[/]','PartieController:createPartie')->setName('post_partie');
