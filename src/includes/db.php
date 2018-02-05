582450853e3ae575ed9007e920bd831bdffd0bc1
Mohamed ALHASNE <alhasne.mohamed@gmail.com>
2018-02-05 13:56:04 +0100

    Database connection / Schema Migration

--- src/includes/db.php
+++ src/includes/db.php
@@ -2,19 +2,19 @@
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-// $db = new Capsule;
+$capsule = new Capsule;
 
-// $capsule->addConnection([
-//     'driver'    => 'mysql',
-//     'host'      => 'db',
-//     'database'  => 'geoquizz',
-//     'username'  => 'geoquizz',
-//     'password'  => 'geoquizz',
-//     'charset'   => 'utf8',
-//     'collation' => 'utf8_general_ci',
-//     'prefix'    => 'geo',
-// ]);
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'geoquizz',
+    'database'  => 'geoquizz',
+    'username'  => 'admin',
+    'password'  => 'admin',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_general_ci',
+    'prefix'    => 'geo',
+]);
 
-// $db->setAsGlobal();
+$capsule->setAsGlobal();
 
-// $db->bootEloquent();
+$capsule->bootEloquent();
