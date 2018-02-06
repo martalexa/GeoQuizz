09005ea5c0fd9bb11b66928c00481f261d78fb6f
nico <bobelet.nicolas@gmail.com>
2018-02-06 16:18:40 +0100

    error handler

--- src/Controllers/PhotoController.php
+++ src/Controllers/PhotoController.php
@@ -13,70 +13,74 @@ use Symfony\Component\Translation\Dumper\PoFileDumper;
 
 class PhotoController extends BaseController
 {
-    function base64_to_jpeg($base64_string, $output_file)
-    {
-        $ifp = fopen($output_file, "wb");
-        fwrite($ifp, base64_decode($base64_string));
-        fclose($ifp);
-        return ($output_file);
-    }
 
-    function check_base64_image($base64, $response)
-    {
+   public function customError($errno, $errstr) {
+      echo "<b>Error:</b> [$errno] $errstr<br>";
+      die;
+  } 
+
+  public function base64_to_jpeg($base64_string, $output_file)
+  {
+    $ifp = fopen($output_file, "wb");
+    fwrite($ifp, base64_decode($base64_string));
+    fclose($ifp);
+    return ($output_file);
+}
+
+public function check_base64_image($base64, $response)
+{
         //todo : desactiver les warnings
-        try {
-            if (imagecreatefromstring(base64_decode($base64))) {
-                return true;
-            } else {
-                throw new \Exception('Format non pris en compte');
-            }
-        } catch (\Exception $e) {
-            return Writer::json_output($response, 403, ["error" => $e->getMessage()]);
-        }
+  set_error_handler($this->customError(2,"Error, try again"));
+    if (imagecreatefromstring(base64_decode($base64))) {
+        return true;
+    } else {
+        return false;
     }
 
-    public function createPhoto($request, $response, $args)
-    {
+}
 
-        $tab = $request->getParsedBody();
+public function createPhoto($request, $response, $args)
+{
 
-        $photo_str = $tab['photo'];
+    $tab = $request->getParsedBody();
 
-
-        $test = $this->check_base64_image($photo_str, $response);
-        if ($test) {
-
-            $photo_str = base64_decode($photo_str);
+    $photo_str = $tab['photo'];
 
 
-            $picture = new Photo();
-            $picture->url = Uuid::uuid1() . '.png';
+    $test = $this->check_base64_image($photo_str, $response);
+    if ($test) {
 
-            if (isset($tab['description']) && !empty($tab['description'])) {
-                $picture->description = filter_var($tab['description'], FILTER_SANITIZE_SPECIAL_CHARS);
-            }
-            $picture->lat = filter_var($tab['lat'], FILTER_SANITIZE_SPECIAL_CHARS);
-            $picture->lng = filter_var($tab['lng'], FILTER_SANITIZE_SPECIAL_CHARS);
-            $picture->serie_id = filter_var($tab['serie_id'], FILTER_SANITIZE_NUMBER_INT);
+        $photo_str = base64_decode($photo_str);
 
-            file_put_contents($this->get('upload_path') . '/' . $picture->url, $photo_str);
-            $picture->url = $this->get('assets_path') . '/uploads/' . $picture->url;
 
-            try {
+        $picture = new Photo();
+        $picture->url = Uuid::uuid1() . '.png';
 
-                $picture->save();
-                return Writer::json_output($response, 201, $picture);
+        if (isset($tab['description']) && !empty($tab['description'])) {
+            $picture->description = filter_var($tab['description'], FILTER_SANITIZE_SPECIAL_CHARS);
+        }
+        $picture->lat = filter_var($tab['lat'], FILTER_SANITIZE_SPECIAL_CHARS);
+        $picture->lng = filter_var($tab['lng'], FILTER_SANITIZE_SPECIAL_CHARS);
+        $picture->serie_id = filter_var($tab['serie_id'], FILTER_SANITIZE_NUMBER_INT);
 
-            } catch (\Exception $e) {
-                $response = $response->withHeader('Content-Type', 'application/json')->withStatus(500);
-                return $response->getBody()->write(json_encode(['type' => 'error', 'error' => 500, 'message' => $e->getMessage()]));
-            }
+        file_put_contents($this->get('upload_path') . '/' . $picture->url, $photo_str);
+       
+
+        try {
+
+            $picture->save();
+             $picture->url = $this->get('assets_path') . '/uploads/' . $picture->url;
+            return Writer::json_output($response, 201, $picture);
+
+        } catch (\Exception $e) {
+            $response = $response->withHeader('Content-Type', 'application/json')->withStatus(500);
+        }
 
 
             //todo: voir pour la redirection sur l'interface graphique
 
-        } else {
-            return Writer::json_output($response,403,['error' => "Forbidden"]);
-        }
+    } else {
+        return Writer::json_output($response,403,['error' => "Forbidden "]);
     }
+}
 }
