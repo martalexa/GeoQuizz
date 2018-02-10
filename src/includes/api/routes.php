6488be4e68735275a14bd0476003c9ee926b9ecf
DanielRicklin <ricklin.daniel@outlook.fr>
2018-02-10 11:23:50 +0100

    apidoc

--- src/includes/api/routes.php
+++ src/includes/api/routes.php
@@ -144,7 +144,7 @@ $app->get('/parties/{id}[/]', 'PartieController:getPartie')->setName('get_partie
  *				"description": "Le musée de l'IUT"
  *				"url": "http://web.geaoquizz.local:10085/uploads/laphoto.png",
  *				"lat": "48.6843900",
- *				"lng": "6.1849600",
+ *				"lng": "6.1849600"
  *			}]
  *		}
  *	}
@@ -152,7 +152,7 @@ $app->get('/parties/{id}[/]', 'PartieController:getPartie')->setName('get_partie
 $app->post('/parties[/]','PartieController:createPartie')->setName('post_partie')->add(\App\Middleware\CheckFormulaire::class.':checkFormulaire')->setArgument('fields',['nb_photos','serie_id','player_username']);
 
 
-//$app->put('/parties/{token}[/]','PartieController:updateScore')->setName('put_score')->add(\App\Middleware\CheckFormulaire::class.':checkFormulaire')->setArgument('fields',['score']);
+$app->put('/parties/{token}[/]','PartieController:updateScore')->setName('put_score')->add(\App\Middleware\CheckFormulaire::class.':checkFormulaire')->setArgument('fields',['score']);
 
 /**
  * @api {put} /partie/:token/state Modification d'une partie
@@ -162,7 +162,7 @@ $app->post('/parties[/]','PartieController:createPartie')->setName('post_partie'
  * @apiHeader {String} Content-Type application/json;charset=utf-8
  * @apiParam {Number} state Etat de la partie
  * @apiParamExample {json} Input
- *	PUT /parties/:token HTTP/1.1
+ *	PUT /parties/:token/state HTTP/1.1
  *	Host: api.geoquizz.local:10080 
  *	Content-Type:application/json;charset=utf-8
  * 	
@@ -196,7 +196,7 @@ $app->patch('/parties/{token}/state','PartieController:updateState')->setName('p
  * @apiHeader {String} Content-Type application/json;charset=utf-8
  * @apiParam {Number} score Score de la partie
  * @apiParamExample {json} Input
- *	PUT /parties/:token HTTP/1.1
+ *	PATCH /parties/:token HTTP/1.1
  *	Host: api.geoquizz.local:10080 
  *	Content-Type:application/json;charset=utf-8
  * 	
@@ -221,7 +221,42 @@ $app->patch('/parties/{token}/state','PartieController:updateState')->setName('p
  *	}
  */
 $app->patch('/parties/{token}/score','PartieController:updateScore')->setName('patch_score')->add(\App\Middleware\CheckFormulaire::class.':checkFormulaire')->setArgument('fields',['score']);
+
+
 // Routes Series
+
+/**
+ * @api {get} /series/ Renvoie toute les series
+ * @apiGroup series
+ * @apiVersion 1.0.0
+ * @apiSuccess {Number} id Identifiant de la serie
+ * @apiSuccess {Number} distance Distance de ref pour le calcul du score
+ * @apiSuccess {String} image Image de la serie
+ * @apiSuccess {String} name Nom de la serie
+ * @apiSuccess {Object} city Description de la ville de référence de la série
+ * @apiSuccess {Number} city.id Identifiant de la ville
+ * @apiSuccess {String} city.name Nom de la ville
+ * @apiSuccess {Number} city.lat Latitude de la ville
+ * @apiSuccess {Number} city.lng Longitude de la ville
+ * @apiSuccess {Number} city.zoom_level Zoom de référence pou la carte leaflet
+ * @apiSuccessExample {json} Success
+ * 	HTTP/1.1 200 OK	
+ *	{
+ *		"id": 17,
+ *		"distance": "52",
+ *		"updated_at": "2018-02-09 18:20:20",
+ *		"created_at": "0000-00-00 00:00:00",
+ *		"image": "http://web.geoquizz.local:10085/uploads/e2f69e70-0dc5-11e8-96d6-0242ac130005.png",
+ *		"name": "La série des musées",
+ *		"city": {
+ *			"id": 1,
+ *			"name": "Nancy",
+ *			"lat": "48.6843900",
+ *			"lng": "6.1849600",
+ *			"zoom_level": 13
+ *		}
+ *	}
+ */
 $app->get('/series[/]', 'SerieController:getSeries')->setName('get_series');
 
 /**
@@ -318,7 +353,7 @@ $app->get('/series/{id: [0-9]+}[/]','SerieController:getSerie')->setName('get_se
  * @apiParam {String} username username de la personne
  * @apiParam {String} password Mot de passe
  * @apiParamExample {json} Input
- *	PUT /parties/:token HTTP/1.1
+ *	POST /admin/signup HTTP/1.1
  *	Host: api.geoquizz.local:10080 
  *	Content-Type:application/json;charset=utf-8
  * 	
@@ -356,24 +391,347 @@ $app->post('/admin/signin[/]', 'UserController:connectUser');
 
 $app->group('/admin', function () {
 
+/**
+ * @api {get} /admin/series/ Renvoie toute les series
+ * @apiGroup series
+ * @apiVersion 1.0.0
+ * @apiHeader {String} Authorization Bearer :token
+ * @apiSuccess {Number} id Identifiant de la serie
+ * @apiSuccess {Number} distance Distance de ref pour le calcul du score
+ * @apiSuccess {String} image Image de la serie
+ * @apiSuccess {String} name Nom de la serie
+ * @apiSuccess {Object} city Description de la ville de référence de la série
+ * @apiSuccess {Number} city.id Identifiant de la ville
+ * @apiSuccess {String} city.name Nom de la ville
+ * @apiSuccess {Number} city.lat Latitude de la ville
+ * @apiSuccess {Number} city.lng Longitude de la ville
+ * @apiSuccess {Number} city.zoom_level Zoom de référence pou la carte leaflet
+ * @apiSuccessExample {json} Success
+ * 	HTTP/1.1 200 OK	
+ *	{
+ *		"id": 17,
+ *		"distance": "52",
+ *		"updated_at": "2018-02-09 18:20:20",
+ *		"created_at": "0000-00-00 00:00:00",
+ *		"image": "http://web.geoquizz.local:10085/uploads/e2f69e70-0dc5-11e8-96d6-0242ac130005.png",
+ *		"name": "La série des musées",
+ *		"city": {
+ *			"id": 1,
+ *			"name": "Nancy",
+ *			"lat": "48.6843900",
+ *			"lng": "6.1849600",
+ *			"zoom_level": 13
+ *		}		
+ *	}
+ */
 	$this->get('/series[/]', 'SerieController:getBackSeries')->setName('get_back_series');
 
+/**
+ * @api {get} /admin/series/:id Renvoie toute les series
+ * @apiGroup series
+ * @apiVersion 1.0.0
+ * @apiHeader {String} Authorization Bearer :token
+ * @apiParams {Number} id Identifiant de al serie
+ * @apiSuccess {Number} id Identifiant de la serie
+ * @apiSuccess {Number} distance Distance de ref pour le calcul du score
+ * @apiSuccess {String} image Image de la serie
+ * @apiSuccess {String} name Nom de la serie
+ * @apiSuccess {Object} city Description de la ville de référence de la série
+ * @apiSuccess {Number} city.id Identifiant de la ville
+ * @apiSuccess {String} city.name Nom de la ville
+ * @apiSuccess {Number} city.lat Latitude de la ville
+ * @apiSuccess {Number} city.lng Longitude de la ville
+ * @apiSuccess {Number} city.zoom_level Zoom de référence pou la carte leaflet
+ * @apiSuccess {Object} photos Tableau d'images
+ * @apiSuccess {Number} photos.id Identifiant de l'image
+ * @apiSuccess {String} photos.description Description de l'image
+ * @apiSuccess {String} photos.url Url de l'image
+ * @apiSuccess {Number} photos.lat Latitude
+ * @apiSuccess {Number} photos.lng Longitude
+ * @apiSuccess {Object} rules Regles pour le score
+ * @apiSuccess {Object} rules.palier
+ * @apiSuccess {Number} rules.palier.id Identifiant du palier
+ * @apiSuccess {Number} rules.palier.points Point pour ce palier
+ * @apiSuccess {Number} rules.palier.coef Coefficient du palier
+ * @apiSuccess {Number} rules.palier.serie_id Serie à laquel le palier appartient
+ * @apiSuccess {Object} rules.times 
+ * @apiSuccess {Number} rules.times.id Identifiant
+ * @apiSuccess {Number} rules.times.nb_seconds Nombre de seconde du palier de temps
+ * @apiSuccess {Number} rules.times.coef Coefficient du palier de temps
+ * @apiSuccess {Number} rules.times.serie_id Serie à laquel le palier appartient
+ * @apiSuccessExample {json} Success
+ * 	HTTP/1.1 200 OK	
+ *	{
+ *		"id": 17,
+ *		"distance": "52",
+ *		"updated_at": "2018-02-09 18:20:20",
+ *		"created_at": "0000-00-00 00:00:00",
+ *		"image": "http://web.geoquizz.local:10085/uploads/e2f69e70-0dc5-11e8-96d6-0242ac130005.png",
+ *		"name": "La série des musées",
+ *		"city": {
+ *			"id": 1,
+ *			"name": "Nancy",
+ *			"lat": "48.6843900",
+ *			"lng": "6.1849600",
+ *			"zoom_level": 13
+ *		},
+ *		"photos": [{
+ *			"id": 4,
+ *			"description": "Le musée de l'IUT"
+ *			"url": "http://web.geaoquizz.local:10085/uploads/laphoto.png",
+ *			"lat": "48.6843900",
+ *			"lng": "6.1849600"
+ *		}],
+ *		"rules": [
+ *			"paliers": [{
+ *				"id": 37,
+ *				"coef": 1,
+ *				"points": 5,
+ *				"serie_id": 17
+ *			}],
+ *			"Times": [{
+ *				"id": 37,
+ *				"nb_seconds": 5,
+ *				"coef": 4,
+ *				"serie_id": 17
+ *			}]			 
+ *		]	
+ *	}
+ */	
 	$this->get('/series/{id: [0-9]+}[/]','SerieController:getSerie')->setName('get_serie');
 
-  $this->post('/series[/]', 'SerieController:createSerie')->setName('post_serie')->add(\App\Middleware\CheckFormulaire::class.':checkFormulaire')->setArgument('fields',['distance'],['name']);
-
+/**
+ * @api {post} /admin/series/ Création d'une serie
+ * @apiGroup series
+ * @apiVersion 1.0.0
+ * @apiHeader {String} Authorization Bearer :token
+ * @apiHeader {String} Content-Type: application/json
+ * @apiParams {String} name Nom de la serie_id
+ * @apiParams {Number} distance Distance max pour score (m)
+ * @apiParams {Object} city Ville référente
+ * @apiParams {Number} city.id Identifiant de la ville référente
+ * @apiParams {String} image Image encodée en base64
+ * @apiParamExample {json} Input
+ *	POST /admin/series HTTP/1.1
+ *	Host: api.geoquizz.local:10080 
+ *	Content-Type:application/json;charset=utf-8
+ * 	
+ *	{
+ *		"name": "la serie",
+ *		"distance": "100",
+ *		"city":{
+ *			"id":1	 
+ *		},
+ *		"image": "/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEB...."
+ *	}
+ * @apiSuccess {Number} id Identifiant de la serie
+ * @apiSuccess {Number} distance Distance de ref pour le calcul du score
+ * @apiSuccess {String} image Image de la serie
+ * @apiSuccess {String} name Nom de la serie
+ * @apiSuccess {Object} city Description de la ville de référence de la série
+ * @apiSuccess {Number} city.id Identifiant de la ville
+ * @apiSuccess {String} city.name Nom de la ville
+ * @apiSuccess {Number} city.lat Latitude de la ville
+ * @apiSuccess {Number} city.lng Longitude de la ville
+ * @apiSuccess {Number} city.zoom_level Zoom de référence pou la carte leaflet
+ * @apiSuccess {Object} rules Regles pour le score
+ * @apiSuccess {Object} rules.palier
+ * @apiSuccess {Number} rules.palier.id Identifiant du palier
+ * @apiSuccess {Number} rules.palier.points Point pour ce palier
+ * @apiSuccess {Number} rules.palier.coef Coefficient du palier
+ * @apiSuccess {Number} rules.palier.serie_id Serie à laquel le palier appartient
+ * @apiSuccess {Object} rules.times 
+ * @apiSuccess {Number} rules.times.id Identifiant
+ * @apiSuccess {Number} rules.times.nb_seconds Nombre de seconde du palier de temps
+ * @apiSuccess {Number} rules.times.coef Coefficient du palier de temps
+ * @apiSuccess {Number} rules.times.serie_id Serie à laquel le palier appartient
+ * @apiSuccessExample {json} Success
+ * 	HTTP/1.1 200 OK	
+ *	{
+ *		"id": 18,
+ *		"distance": "52",
+ *		"image": "http://web.geoquizz.local:10085/uploads/e2f69e70-0dc5-11e8-96d6-0242ac130005.png",
+ *		"name": "La série des musées",
+ *		"city": {
+ *			"id": 1,
+ *			"name": "Nancy",
+ *			"lat": "48.6843900",
+ *			"lng": "6.1849600",
+ *			"zoom_level": 13
+ *		},
+ *		"rules": [
+ *			"paliers": [{
+ *				"id": 37,
+ *				"coef": 1,
+ *				"points": 5,
+ *				"serie_id": 17
+ *			}],
+ *			"Times": [{
+ *				"id": 37,
+ *				"nb_seconds": 5,
+ *				"coef": 4,
+ *				"serie_id": 17
+ *			}]			 
+ *		]	
+ *	}
+ */	
+  	$this->post('/series[/]', 'SerieController:createSerie')->setName('post_serie')->add(\App\Middleware\CheckFormulaire::class.':checkFormulaire')->setArgument('fields',['distance'],['name']);
+/**
+ * 
+ * @api {delete} /admin/series/:id Suppression d'une série
+ * @apiGroup group
+ * @apiVersion  1.0.0
+ */
 	$this->delete('/series/{id: [0-9]+}[/]', 'SerieController:deleteSeries')->setName('delete_serie');
 
+/**
+ * @api {post} /admin/series/:id/photos Ajout de photo à la serie
+ * @apiGroup series
+ * @apiVersion 1.0.0
+ * @apiHeader {String} Authorization Bearer :token
+ * @apiHeader {String} Content-Type: application/json
+ * @apiParams {String} name Nom de la serie_id
+ * @apiParams {Number} distance Distance max pour score (m)
+ * @apiParams {Object} city Ville référente
+ * @apiParams {Number} city.id Identifiant de la ville référente
+ * @apiParams {String} image Image encodée en base64
+ * @apiParamExample {json} Input
+ *	POST /admin/series/:id/photos HTTP/1.1
+ *	Host: api.geoquizz.local:10080 
+ *	Content-Type:application/json;charset=utf-8
+ * 	
+ *	{
+ *		"photo": "/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEB....",
+ *		"lat": "48.693547842429",
+ *		"lng": "6.1606693267822",
+ *		"description": "L'image du l'IUT"
+ *	}
+ * @apiSuccess {String} url Url de l'image
+ * @apiSuccess {Number} lat Latitude du lieu de l'image
+ * @apiSuccess {Number} lng Longitude du lieu de l'image
+ * @apiSuccess {Number} serie_id Identifiant de la série
+ * @apiSuccess {Object} description Description de l'image
+ * @apiSuccess {Number} id Identifiant de l'image'
+ * @apiSuccessExample {json} Success
+ * 	HTTP/1.1 200 OK	
+ *	{
+ *		"url":"http:\/\/web.geoquizz.local:10085\/uploads\/b55a9738-0e41-11e8-8fec-0242ac130004.png",
+ *		"lat":"48.693547842429"
+ *		"lng":"6.1606693267822",
+ *		"serie_id":17,
+ *		"description":"EAEGG",
+ *		"id":20
+ *	}
+ */
 	$this->post('/series/{id: [0-9]+}/photos[/]', 'PhotoController:createPhoto')->add(\App\Middleware\CheckFormulaire::class.':checkFormulaire')->setArgument('fields',['photo','lat','lng']);
 
+/**
+ * @api {patch} /admin/series/:id/palier Ajout de palier
+ * @apiGroup regle
+ * @apiVersion 1.0.0
+ * @apiHeader {String} Authorization Bearer :token
+ * @apiHeader {String} Content-Type: application/json
+ * @apiParams {Object} paliers
+ * @apiParams {Number} paliers.coef Coefficient du palier
+ * @apiParams {Number} paliers.points Point du palier
+ * @apiParamExample {json} Input
+ * 	
+ *	{
+ *		"paliers":{
+ *			"coef": 1,
+ *			"points": 15
+ *		}
+ *	}
+ * @apiSuccess {String} url Url de l'image
+ * @apiSuccess {Number} lat Latitude du lieu de l'image
+ * @apiSuccess {Number} lng Longitude du lieu de l'image
+ * @apiSuccess {Number} serie_id Identifiant de la série
+ * @apiSuccess {Object} description Description de l'image
+ * @apiSuccess {Number} id Identifiant de l'image'
+ * @apiSuccessExample {json} Success
+ * 	HTTP/1.1 200 OK	
+ *	{
+ *		"serie_id":20,
+ *		"coef":1
+ *		"points":50,
+ *	}
+ */
 	$this->patch('/series/{id: [0-9]+}/paliers[/]', 'PalierController:createPalier');
 
+
+/**
+ * @api {patch} /admin/series/:id/edit Modification d'une série
+ * @apiGroup series
+ * @apiVersion 1.0.0
+ * @apiHeader {String} Authorization Bearer :token
+ * @apiHeader {String} Content-Type: application/json
+ * @apiParams {Number} city_id Identifiant de la ville
+ * @apiParams {String} name Nom de la série
+ * @apiParams {Number} distance Distance de ref pour le score
+ * @apiParams {String} image Image
+ * @apiParamExample {json} Input
+ * 	
+ *	{
+ *		"city_id":1,
+ *		"name": "Le nouveau nom",
+ *		"distance": 150,
+ *		"image": "/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEB...."
+ *	}
+ * @apiSuccessExample {json} Success
+ * 	Like /admin/series/:id
+ */
 	$this->patch('/series/{id: [0-9]+}/edit[/]', 'SerieController:editSerie')->add(\App\Middleware\CheckFormulaire::class.':checkFormulaire')->setArgument('fields',['image','city_id','distance']);
 
+/**
+ * @api {patch} /admin/series/:id/times Ajout d'un timer
+ * @apiGroup series
+ * @apiVersion 1.0.0
+ * @apiHeader {String} Authorization Bearer :token
+ * @apiHeader {String} Content-Type: application/json
+ * @apiParams {Object} times Identifiant de la ville
+ * @apiParams {Number} times.coef coef du timer
+ * @apiParams {String} times.nb_seconds Nombre de seconde pour ce timer
+ * @apiParamExample {json} Input
+ * 	
+ *	{
+ *		"times":{
+ *			"coef":1,
+ *			"nb_seconds": "Le nouveau nom"
+ *		}
+ *	}
+ * @apiSuccessExample {json} Success
+ * 	Like /admin/series/:id
+ */
 	$this->patch('/series/{id: [0-9]+}/times[/]', 'TimeController:createTime');
+
 
 	$this->put('/series/{id: [0-9]+}/rules[/]', 'RulesController:modifyRules');
 
+/**
+ * @api {get} /admin/cities Renvoie les villes
+ * @apiGroup cities
+ * @apiVersion 1.0.0
+ * @apiHeader {String} Authorization Bearer :token
+ * @apiSuccess {Number} id Identifiant de la serie
+ * @apiSuccess {Number} distance Distance de ref pour le calcul du score
+ * @apiSuccess {String} image Image de la serie
+ * @apiSuccess {String} name Nom de la serie
+ * @apiSuccess {Object} city Description de la ville de référence de la série
+ * @apiSuccess {Number} city.id Identifiant de la ville
+ * @apiSuccess {String} city.name Nom de la ville
+ * @apiSuccess {Number} city.lat Latitude de la ville
+ * @apiSuccess {Number} city.lng Longitude de la ville
+ * @apiSuccess {Number} city.zoom_level Zoom de référence pou la carte leaflet
+ * @apiSuccessExample {json} Success
+ * 	HTTP/1.1 200 OK	
+ *	{
+ *		"id": 1,
+ *		"name": "Nancy",
+ *		"lat": "48.6843900",
+ *		"lng": "6.1849600",
+ *		"zoom_level": 13		
+ *	}
+ */
 	$this->get('/cities[/]','CityController:getCities')->setName('get_cities');
 
 })->add(new \App\Middleware\CheckJwt($container));
